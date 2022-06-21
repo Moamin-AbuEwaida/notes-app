6df4c17f906837ad045b63ceb1fb80c0b89f2e46
Moamin <moamin.salamah@gmail.com>
2022-06-21 18:27:09 +0200

    bugs still there

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,8 +4,8 @@ import { useRouter } from "vue-router";
 import { useSignInEmailPassword, useSignUpEmailPassword } from "@nhost/vue";
 
 const router = useRouter();
-const isRegister = ref(false);
 
+const isRegister = ref(false);
 const email = ref("");
 const password = ref("");
 
@@ -13,15 +13,12 @@ const { signUpEmailPassword } = useSignUpEmailPassword();
 const { signInEmailPassword } = useSignInEmailPassword();
 
 const registerOrLogin = async () => {
-  if (!email.value || !password.value) {
+  if (!email.value || !password.value)
     return alert("Please fill in all fields");
-  }
   const res = isRegister.value
     ? await signUpEmailPassword(email.value, password.value)
     : await signInEmailPassword(email.value, password.value);
-
   if (res.isError) return alert(res.error.message);
-
   router.push("/");
 };
 </script>
@@ -31,8 +28,44 @@ const registerOrLogin = async () => {
     <h1 class="text-4xl font-bold mb-8">My Notes App</h1>
 
     <form @submit.prevent="registerOrLogin">
-      <h3>Login or Register</h3>
-      <label><input type="checkbox" v-model="isRegister" /> Register ?</label>
+      <h3 class="text-xl font-bold uppercase mb-4">
+        {{ isRegister ? "Register" : "Login" }} Form
+      </h3>
+
+      <label class="block mb-4">
+        <span class="block text-sm uppercase mb-2">Email</span>
+        <input
+          type="email"
+          v-model="email"
+          class="block w-full text-slate-800 px-4 py-2"
+        />
+      </label>
+
+      <label class="block mb-4">
+        <span class="block text-sm uppercase mb-2">Password</span>
+        <input
+          type="password"
+          v-model="password"
+          class="block w-full text-slate-800 px-4 py-2"
+        />
+      </label>
+
+      <input
+        type="submit"
+        :value="isRegister ? 'Register' : 'Login'"
+        class="text-green-500 hover:underline mb-4 cursor-pointer"
+      />
+
+      <p>
+        Need to
+        <button
+          type="button"
+          @click="() => (isRegister = !isRegister)"
+          class="text-green-500 hover:underline"
+        >
+          {{ isRegister ? "Login" : "Register" }}</button
+        >?
+      </p>
     </form>
   </main>
 </template>
